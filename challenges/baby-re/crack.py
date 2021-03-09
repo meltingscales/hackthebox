64c5290e78c7124db22799ee6ab43297a9379388
Henry Post <HenryFBP@gmail.com>
2021-03-08 21:23:12 -0600

    add some byte fuzzing

--- challenges/baby-re/crack.py
+++ challenges/baby-re/crack.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
 from subprocess import Popen, PIPE
+from pprint import pprint
+
+def try_password(input):
+    process = Popen(['./baby'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    process.communicate(input=input)
+    stdout, stderr = process.communicate()
+
+input = b'(.*)'
+
+# range of all bytes
+for i in range(1, 256):
+    input += i.to_bytes(1,'big')
+
+input += b'\n'
+
+print("input:")
+pprint(input)
 
 process = Popen(['./baby'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-process.communicate(input=b'password\n')
-stdout, stderr = process.communicate()
-print (stdout)
+stdout, stderr = process.communicate(input=input)
+print (stderr.decode('utf-8'))
+print (stdout.decode('utf-8'))
