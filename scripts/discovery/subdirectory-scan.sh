80c4b8b6db0f014d2c126d82fbbe4aca3abbf78b
HenryFBP <HenryFBP@gmail.com>
2022-01-01 17:21:26 +0000

    add wfuzz

--- scripts/discovery/subdirectory-scan.sh
+++ scripts/discovery/subdirectory-scan.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 
 : ${1?" Error.
-Usage: $0 <target> [protocol=http].
-You forgot to supply a target"}
+Usage: $0 <TARGET> [protocol] [output_dir].
+You forgot to supply a TARGET"}
 
-target=$1
-PROTOCOL=${2:-http} #optional arg
+TARGET=$1
+PROTOCOL=${2:-http} # optional args
+OUTPUT_DIR=${3:-output}
 
-echo "Writing to a series of files like '$target.*' in the current directory."
+echo "Writing to a series of files like '$TARGET.*.txt' in the directory '$OUTPUT_DIR'."
 echo "You can view them live with 'tail -f FILE'."
+echo "This might take a while, consider running this in a separate terminal."
 
-dirb $PROTOCOL://$target    -o $target.dirb.txt     >/dev/null 2>&1 & 
-dirb $PROTOCOL://localhost  -o localhost.dirb.txt   >/dev/null 2>&1 &
+if [[ ! -d $OUTPUT_DIR ]]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
+dirb $PROTOCOL://$TARGET    -o $OUTPUT_DIR/$TARGET.dirb.txt     >/dev/null 2>&1 & 
+
+wfuzz --sc 200 --oF $OUTPUT_DIR/$TARGET.wfuzz-directory-list-1.0.txt -w ~/Git/SecLists/Discovery/Web-Content/directory-list-1.0.txt $PROTOCOL://$TARGET/FUZZ    >/dev/null 2>&1 &
 
 FAIL=0
 
 for job in `jobs -p` #for all background processes,
 do
 echo "wait on job $job"
-    wait $job || let "FAIL+=1"
+    wait $job || let "FAIL+=1" #wait for it to exit, or, if it exits non-zero, increment FAIL
 done
 
-if [ "$FAIL" == "0" ];
+if [ "$FAIL" == "0" ]; # did 1 job tail?
 then
     echo "Done!"
 else
