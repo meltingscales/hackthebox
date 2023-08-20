6cd1c72fdb473a0f0f3b5831efcee380d8f3d097
Henry Post <henryfbp@gmail.com>
2023-08-20 21:52:06 +0100

    work on cybermonday

--- boxes/CyberMonday/rockyou-post-login.py
+++ boxes/CyberMonday/rockyou-post-login.py
@@ -1,1 +1,19 @@
 # admin, admin@cybermonday.htb
+
+
+import requests
+
+bot = requests.session()
+
+bot.get('http://cybermonday.htb/')
+
+response = bot.post(
+    "http://cybermonday.htb/login",
+    {
+        # 'username': "admin",
+        'email': "admin@cybermonday.htb",
+        'password': 'test',
+        'remember': 'on'
+    })
+
+print(response)
