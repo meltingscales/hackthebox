79038659e0e9a9449cbf0fd98464373634a7d843
Henry Post <HenryFBP@gmail.com>
2025-06-06 15:07:02 -0500

    solve dynastic

---
+++ ctfs/CTF Try Out/Dynastic.solved/crypto_dynastic/source.py
@@ -0,0 +1,34 @@
+FLAG='DJF_CTA_SWYH_NPDKK_MBZ_QPHTIGPMZY_KRZSQE?!_ZL_CN_PGLIMCU_YU_KJODME_RYGZXL'
+
+def to_identity_map(a):
+    return ord(a) - 0x41
+
+def from_identity_map(a):
+    return chr(a % 26 + 0x41)
+
+def encrypt(m):
+    c = ''
+    for i in range(len(m)):
+        ch = m[i]
+        if not ch.isalpha():
+            ech = ch
+        else:
+            chi = to_identity_map(ch)
+            ech = from_identity_map(chi + i)
+        c += ech
+    return c
+
+def decrypt(m):
+    c = ''
+    for i in range(len(m)):
+        ch = m[i]
+        if not ch.isalpha():
+            ech = ch
+        else:
+            chi = to_identity_map(ch)
+            ech = from_identity_map(chi - i) # this minus is the only difference
+        c += ech
+    return c
+
+
+print(f"HTB{"{"}{decrypt(FLAG)}{"}"}")
---
+++ ctfs/CTF Try Out/Dynastic.solved/notes.md
@@ -0,0 +1,2 @@
+It looks like this is just a caesar cipher, from asking Gemini.
+
